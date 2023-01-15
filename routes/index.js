210aaa22291e2bbb74f0e998fbbc0aeae1193830
jaroddurkin <jarod@jdurkin.net>
2023-01-15 17:22:33 -0500

    Make express app more simple

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+/* Sample Hello World Endpoint */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({ message: "Hello World!"});
 });
 
 module.exports = router;
