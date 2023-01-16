68e80c74618d2019f0c0b66763d48b74f8c5a5e0
Ricky102 <48187498+Ricky102@users.noreply.github.com>
2023-01-15 20:04:47 -0500

    Using connect.js as a controller for db functions. Attempting to return user data from db when visit /users route.

--- api/db/connect.js
+++ api/db/connect.js
@@ -1,3 +1,6 @@
+//This file contains code to connect to a local PostgreSQL database.
+//It also serves as a controller for db related tasks/functions
+
 require('dotenv').config()
 const {Client} = require('pg')
 
@@ -10,32 +13,36 @@ const client = new Client({
 })
 client.connect();
 
-const getUsers = (req, res) => {
-    client.query(`SELECT * FROM userstest`, (err, res) => {
+//Queries stored as var and passed into functions instead of string to avoid sql injection vulnerability.
+
+const getUsers = (req) => {
+    selectUsers = `SELECT * FROM userstest`
+    client.query(selectUsers, (err, res) => {
         if(!err){
-            console.log(res.rows);
-            //res.status(200).json({ user: res.rows })
+            console.log(res.rows)
+            return res.status(200).json({ users: res.rows })
         } else {
             console.log(err.message)
         }
-        client.end();
+        client.end()
     })
 }
 
-const getUsersById = (req, res) => {
-    client.query(`SELECT * FROM userstest WHERE userid=${req.params.id}`, (err, res) => {
+const getUserById = (req) => {
+    selectUserById = `SELECT * FROM userstest WHERE userid=${req.params.id}`
+    client.query(selectUserById, (err, res) => {
         if(!err){
-            console.log(res.rows);
-            //res.status(200).json({ user: res.rows })
+            console.log(res.rows)
+            res.status(200).json({ user: res.rows })
         } else {
             console.log(err.message)
         }
-        client.end();
+        client.end()
     })
 }
 
 //module.exports = client;
 module.exports = {
     getUsers,
-    getUsersById
+    getUserById
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const {
+  getUsers,
+  getUserById
+} = require('../db/connect.js');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  getUsers();
 });
+
+router.route('/:id').get(getUserById);
 
 module.exports = router;
