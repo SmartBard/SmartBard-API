9107fb6d57612926cec796087117d7c542e29f72
Ricky102 <48187498+Ricky102@users.noreply.github.com>
2023-02-10 23:45:36 -0500

    put endpoint implemented for user settings. Still getting HTTP HEADER error and need to add validation to get endpoint.

--- db/db-user-settings-interface.js
+++ db/db-user-settings-interface.js
@@ -1,18 +1,30 @@
 const {
-    executeQuery
+  executeQuery
 } = require('./connect');
 
 async function getUserSettings() {
-    const selectUserSettings = `SELECT * FROM user_settings`;
-    return await executeQuery(selectUserSettings);
+  const selectUserSettings = `SELECT * FROM user_settings`;
+  return await executeQuery(selectUserSettings);
+}
+
+async function getUserSettingsById(settingsId) {
+  const selectSettingById = `SELECT * FROM user_settings WHERE settings_id = ${settingsId}`;
+  return await executeQuery(selectSettingById);
 }
 
 async function createUserSettings(columns, values) {
-    const insertUserSettings = `INSERT INTO user_settings (${columns}) VALUES (${values})`;
-    return await executeQuery(insertUserSettings);
+  const insertUserSettings = `INSERT INTO user_settings (${columns}) VALUES (${values})`;
+  return await executeQuery(insertUserSettings);
+}
+
+async function updateUserSettings(column, newValue, settingsId) {
+  const setUserSettings = `UPDATE user_settings SET ${column} = ${newValue} WHERE settings_id = ${settingsId}`;
+  return await executeQuery(setUserSettings);
 }
 
 module.exports = {
-    getUserSettings,
-    createUserSettings
+  getUserSettings,
+  getUserSettingsById,
+  createUserSettings,
+  updateUserSettings
 }
--- routes/user-settings.js
+++ routes/user-settings.js
@@ -1,48 +1,78 @@
 const express = require('express');
 const router = express.Router();
 const {
-    getUserSettings,
-    createUserSettings
+  getUserSettings,
+  getUserSettingsById,
+  createUserSettings,
+  updateUserSettings
 } = require('../db/db-user-settings-interface');
 
 const POST_BODY_FORMAT = {
-    "text_size": "number",
-    "brightness": "number",
-    "contrast": "number",
-    "volume": "number",
-    "primary_color": "string"
+  "text_size": "number",
+  "brightness": "number",
+  "contrast": "number",
+  "volume": "number",
+  "primary_color": "string"
 }
 
 // get endpoint for user settings
 router.get('/', async function(req, res, next) {
-    getUserSettings().then((query) => {
-        res.status(200).send(query.rows);
-    }).catch((err) => {
-        res.status(400).send({ error: 'Unknown error.' });
-    });
+  // console.log(req.query);
+  getUserSettings().then((query) => {
+    res.status(200).send(query.rows);
+  }).catch((err) => {
+    res.status(400).send({ error: 'Unknown error.' });
+  });
 });
 
-// post endpoint, will replace values with req.body.text_size, req.body.brightness etc. for dynamic query
+// post endpoint
 router.post('/', async function(req, res, next) {
-    for (const prop in POST_BODY_FORMAT) {
-        if (!(prop in req.body)) {
-            res.status(400).send({ error: `Did not find required property: ${prop}` });
-            return;
-        }
-        if (typeof req.body[prop] !== POST_BODY_FORMAT[prop]) {
-            res.status(400).send({ error: `Invalid type for property: ${prop}. Expected type: ${POST_BODY_FORMAT[prop]}` });
-            return;
-        }
+  for (const prop in POST_BODY_FORMAT) {
+    if (!(prop in req.body)) {
+      res.status(400).send({ error: `Did not find required property: ${prop}` });
+      return;
     }
-    const fields = 'text_size, brightness, contrast, volume, primary_color';
-    const values = `${req.body.text_size}, ${req.body.brightness}, ${req.body.contrast}, ${req.body.volume}, '${req.body.primary_color}'`;
-    createUserSettings(fields, values).then((query) => {
-        res.status(200).send({ command: query.command });
-    }).catch((err) => {
-        res.status(400).send({ error: 'Unknown error.' });
-    });
+    if (typeof req.body[prop] !== POST_BODY_FORMAT[prop]) {
+      res.status(400).send({ error: `Invalid type for property: ${prop}. Expected type: ${POST_BODY_FORMAT[prop]}` });
+      return;
+    }
+  }
+  const fields = 'text_size, brightness, contrast, volume, primary_color';
+  const values = `${req.body.text_size}, ${req.body.brightness}, ${req.body.contrast}, ${req.body.volume}, '${req.body.primary_color}'`;
+  createUserSettings(fields, values).then((query) => {
+    res.status(200).send({ command: query.command });
+  }).catch((err) => {
+    res.status(400).send({ error: 'Unknown error.' });
+  });
 })
 
 // put endpoint
+router.put('/:settingsId', async function(req, res, next) {
+  for (const prop in req.body) {
+    if (req.body.hasOwnProperty(prop)) {
+      if (!(prop in POST_BODY_FORMAT)) {
+        res.status(400).send({ error: `Invalid body property: ${prop}`});
+        return;
+      } else {
+        // If req.body[prop] is string and not encased in quotes, error will return..
+        let newValue = req.body[prop];
+        if (typeof req.body[prop] === 'string') {
+          newValue = `'${req.body[prop]}'`;
+        } 
+        await updateUserSettings(prop, newValue, req.params.settingsId).catch((err) => {
+          res.status(500).send({ error: 'Unknown error.' });
+        });
+        if (res.statusCode == 500) {
+          return;
+        }
+      }
+    }
+    getUserSettingsById(req.params.settingsId).then((query) => {
+      res.status(200).send({ settings_id: query.rows[0].settings_id, update: "success" });
+    }).catch((err) => {
+      res.status(500).send({ error: 'Unknown error.' });
+    });
+  }
+});
 
 module.exports = router;
