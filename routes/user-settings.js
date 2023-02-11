4e1a4ed48203a0c42f7a27b851900167a031eb4a
Ricky102 <48187498+Ricky102@users.noreply.github.com>
2023-02-10 20:43:03 -0500

    post endpoint for user settings implemented with error checking and using dynamic queries.

--- routes/user-settings.js
+++ routes/user-settings.js
@@ -5,6 +5,14 @@ const {
     createUserSettings
 } = require('../db/db-user-settings-interface');
 
+const POST_BODY_FORMAT = {
+    "text_size": "number",
+    "brightness": "number",
+    "contrast": "number",
+    "volume": "number",
+    "primary_color": "string"
+}
+
 // get endpoint for user settings
 router.get('/', async function(req, res, next) {
     getUserSettings().then((query) => {
@@ -16,11 +24,20 @@ router.get('/', async function(req, res, next) {
 
 // post endpoint, will replace values with req.body.text_size, req.body.brightness etc. for dynamic query
 router.post('/', async function(req, res, next) {
+    for (const prop in POST_BODY_FORMAT) {
+        if (!(prop in req.body)) {
+            res.status(400).send({ error: `Did not find required property: ${prop}` });
+            return;
+        }
+        if (typeof req.body[prop] !== POST_BODY_FORMAT[prop]) {
+            res.status(400).send({ error: `Invalid type for property: ${prop}. Expected type: ${POST_BODY_FORMAT[prop]}` });
+            return;
+        }
+    }
     const fields = 'text_size, brightness, contrast, volume, primary_color';
-    const values = "11, 10, 7, 3, 'dark mode'";
+    const values = `${req.body.text_size}, ${req.body.brightness}, ${req.body.contrast}, ${req.body.volume}, '${req.body.primary_color}'`;
     createUserSettings(fields, values).then((query) => {
-        // res.status(200).send({ settingId: query.rows[0].setting_id });
-        res.status(200).send(query.rows);
+        res.status(200).send({ command: query.command });
     }).catch((err) => {
         res.status(400).send({ error: 'Unknown error.' });
     });
