402c4e0973232dff8fd2e5cb609f803491b8c476
jaroddurkin <jarod@jdurkin.net>
2023-05-10 00:04:54 -0400

    add endpoints to users so UI can determine admins

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const cloudWatchLogger = require('../services/log/cloudwatch');
 const {getUserById, getUserByEmail} = require("../db/db-user-interface");
+const tokenValidator = require('../services/auth/token-validation');
 
 router.get('/', async function(req, res, next) {
     if (!req.query.hasOwnProperty("id") && !req.query.hasOwnProperty("email")) {
@@ -29,6 +30,45 @@ router.get('/', async function(req, res, next) {
     if (res.statusCode === 500) {
         return;
     }
+    if (Object.keys(user).length !== 0) {
+        res.status(200).send(user);
+    } else {
+        res.status(404).send({ error: 'User not found.' });
+    }
+});
+
+router.get('/self', async function (req, res, next) {
+    const email = await tokenValidator.getUserEmailFromToken(req.header('Authorization').split(' ')[1]); // hardcoded but should be valid due to middleware
+    let userId = await getUserByEmail(email).then((query) => {
+        if (query.rows.length > 0) {
+            return query.rows[0].userid;
+        } else {
+            return "";
+        }
+    }).catch((err) => {
+        res.status(500).send({ error: 'Unknown error' });
+        cloudWatchLogger.logger.error(err);
+        console.log(err);
+        return "";
+    });
+    if (res.statusCode === 500) {
+        return;
+    }
+
+    const user = await getUserById(userId).then((query) => {
+        return query.rows.length > 0 ? query.rows[0] : {};
+    });
+    if (Object.keys(user).length !== 0) {
+        res.status(200).send(user);
+    } else {
+        res.status(404).send({ error: 'User not found.' });
+    }
+});
+
+router.get('/:userId', async function (req, res, next) {
+    const user = await getUserById(req.params.userId).then((query) => {
+        return query.rows.length > 0 ? query.rows[0] : {};
+    });
     if (Object.keys(user).length !== 0) {
         res.status(200).send(user);
     } else {
